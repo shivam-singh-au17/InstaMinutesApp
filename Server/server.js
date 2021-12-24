c1004498fb18dc4406480d487457d3219a734d34
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-24 21:50:37 +0530

    Connection established by socket.io

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,29 @@
 require("dotenv").config();
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, "./../Public");
-var app = express();
-
-const port = process.env.PORT;
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT
+let app = express();
+let server = http.createServer(app);
+let ioServer = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-    console.log("Express server running on PORT :=>", port);
+ioServer.on("connection", (socket) => {
+
+    console.log("A new user just connected");
+
+    socket.on("disconnect", () => {
+        console.log("User was disconnected");
+    })
 })
+
+
+
+server.listen(port, () => {
+    console.log("Express server running on PORT :=>", port);
+});
 
