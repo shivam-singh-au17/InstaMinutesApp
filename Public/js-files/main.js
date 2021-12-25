401ea15bdb39ca510196505efaefbb1e831daf9e
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-25 09:04:44 +0530

    Added messaging functionality by multiple users

--- Public/js-files/main.js
+++ Public/js-files/main.js
@@ -11,12 +11,28 @@ socket.on("disconnect", () => {
 
 socket.on("newMessage", (message) => {
     console.log("New Message", message);
+    let li = document.createElement("li");
+    li.innerHTML = `${message.from} : ${message.text}`
+    document.querySelector("body").appendChild(li);
 })
 
 
-socket.emit("createMessage", {
-    from: "Shivam Singh",
-    text: "Hey"
-}, function (message) {
-    console.log("Got it.", message);
-});
+// socket.emit("createMessage", {
+//     from: "Shivam Singh",
+//     text: "Hey"
+// }, function (message) {
+//     console.log("Got it.", message);
+// });
+
+
+
+document.getElementById('submit-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    socket.emit("createMessage", {
+        from: "User",
+        text: document.querySelector('input[name="message"]').value
+    }, function () {
+        
+    })
+})
